e59dc0bb9c3e58d07671d5135c4b49e6b005fc29
Barrett <bartucker93@gmail.com>
2025-12-26 20:31:09 -0500

    day 6 p2 solution
    Saving the numbers and whitespaces for p2 into their own list. Padding each string in the list to be same length and then reversing the string to get the correct ints for the equation.

--- day6.py
+++ day6.py
@@ -17,6 +17,7 @@ cur_col = 0
 
 while cur_col < max_col:
     nums = []
+    nums_with_spaces = []
     max_problem_col_index = cur_col
 
     for row_index in range(num_rows - 1):
@@ -24,6 +25,7 @@ while cur_col < max_col:
         row = worksheet[row_index]
         col_index = cur_col
         num = ""
+        num_with_space = ""
 
         if col_index < len(row):
             cont_search = True
@@ -31,6 +33,7 @@ while cur_col < max_col:
             while cont_search:
                 if col_index < len(row):
                     char = row[col_index]
+                    num_with_space += char
                     if char == " " and num != "":
                         #blank space was found and we have a number. go to next row
                         #after adding num to list
@@ -48,6 +51,8 @@ while cur_col < max_col:
                         nums.append(int(num))
                 col_index += 1
 
+        nums_with_spaces.append(num_with_space)
+
     if nums:
         #grabbing the operator from the last line now that we have all the numbers going into the equation
         operator = worksheet[-1][cur_col:max_problem_col_index].strip()
@@ -59,9 +64,32 @@ while cur_col < max_col:
                 product *= n
             p1_total += product
 
+    block_width = max(len(num_with_space) for num_with_space in nums_with_spaces)
+    padded_nums = [num_with_space.ljust(block_width) for num_with_space in nums_with_spaces]
+
+    p2_nums = []
+    for char_index in range(block_width - 1, -1, -1):
+        vert_num_str = ""
+        for row_index in range(len(padded_nums)):
+            char = padded_nums[row_index][char_index]
+            if char != " ":
+                vert_num_str += char
+
+        if vert_num_str:
+            p2_nums.append(int(vert_num_str))
+
+    if operator == "+":
+        p2_total += sum(p2_nums)
+    elif operator == "*":
+        p2_prod = 1
+        for n in p2_nums:
+            p2_prod *= n
+        p2_total += p2_prod
+
     if max_problem_col_index > cur_col:
         cur_col = max_problem_col_index
     else:
         cur_col += 1
 
 print(f'The answer to the homework in part 1 is: {p1_total}')
+print(f'The answer to the homework in part 2 is: {p2_total}')
